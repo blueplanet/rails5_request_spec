496eac42bc81e0b0a9987b9705c75880b199ab59
blueplanet <erguolinge@gmail.com>
2016-10-14 09:27:08 +0900

    fix spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,14 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    before do
-      User.create name: 'user1'
-      User.create name: 'user2'
+    let!(:users) do
+      [
+        User.create(name: 'user1'),
+        User.create(name: 'user2')
+      ]
     end
 
     it do
       is_expected.to eq(200)
-      expect(response.body).to eq('aa')
+      expect(response.body).to eq(users.map(&:as_json).to_json)
+    end
+  end
+
+  describe 'POST /users' do
+    let(:params) {
+      { user: { name: 'test user' } }
+    }
+
+    it do
+      is_expected.to eq(201)
     end
   end
 end
